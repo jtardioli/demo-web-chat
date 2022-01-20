10ff72ca93c5a1ca2c9529e00580066a085da81b
josht <85530348+jtardioli@users.noreply.github.com>
2022-01-20 17:02:19 -0500

    Initialize 0-start

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8002;
+
+app.use(express.static("public"));
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
